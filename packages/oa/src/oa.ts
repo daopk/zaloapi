e0546fb19962f1effafb40e2593996b086b5540e
Phan Khắc Đạo <daopk.dev@gmail.com>
2023-08-16 15:28:20 +0700

    Update OAInfo interface

--- packages/oa/src/oa.ts
+++ packages/oa/src/oa.ts
@@ -2,17 +2,30 @@ import { fetch } from '@zaloapi/shared'
 import { openapiV2BaseURL } from './constant'
 import type { ZaloOAResponse } from './response'
 
-export type GetOAResponse = ZaloOAResponse<{
+export interface OAInfo {
   oa_id: string
-  description: string
   name: string
+  description: string
+  oa_alias?: string
+  is_verified: boolean
+  /**
+   * Loại OA:
+   * - 2 - OA Doanh nghiệp
+   * - 4 - OA Cơ quan nhà nước
+   */
+  oa_type: number
+  cate_name: string
+  num_follower: number
   avatar: string
   cover: string
-  is_verified: boolean
-}>
+  package_name: string
+  package_valid_through_date?: string
+  package_auto_renew_date?: string
+  linked_zca?: string
+}
 
 export function getOAProfile(access_token: string) {
-  return fetch<GetOAResponse>('getoa', {
+  return fetch<ZaloOAResponse<OAInfo>>('getoa', {
     baseURL: openapiV2BaseURL,
     headers: { access_token },
   })
