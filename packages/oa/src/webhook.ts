e83b1c1172cc6f8ae86648968d2d56b2cb8c0b0b
Phan Khắc Đạo <daopk.dev@gmail.com>
2023-10-17 12:22:12 +0700

    Use original params to generate signature

--- packages/oa/src/webhook.ts
+++ packages/oa/src/webhook.ts
@@ -3,7 +3,7 @@ import { sha256 } from 'ohash'
 export function generateSignature(appId: string, data: string, timestamp: string, oASecretKey: string) {
   const parts: string[] = [appId, data, timestamp, oASecretKey]
 
-  const hash = sha256(parts.map(s => s.trim()).join(''))
+  const hash = sha256(parts.join(''))
 
   return `mac=${hash}`
 }
