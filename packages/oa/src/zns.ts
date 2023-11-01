e20317b0d73ae8698d0ccb907b5167f64036cf0c
Phan Khắc Đạo <daopk.dev@gmail.com>
2023-11-01 10:23:48 +0700

    Add more ZNS apis

--- packages/oa/src/zns.ts
+++ packages/oa/src/zns.ts
@@ -72,3 +72,116 @@ export function sendZNSTemplateMessage(
     headers: { access_token },
   })
 }
+
+export function getZNSQuota(access_token: string) {
+  return fetch<ZaloOAResponse<{
+    dailyQuota: number
+    remainingQuota: number
+  }>>('message/quota', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+  })
+}
+
+export function getZNSMessageStatus(access_token: string, message_id: string) {
+  return fetch<ZaloOAResponse<{
+    delivery_time: string
+    message: string
+    status: number
+  }>>('message/status', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+    params: {
+      message_id,
+    },
+  })
+}
+
+export type ZNSMessageTemplateTag = 'OTP' | 'IN_TRANSACTION' | 'POST_TRANSACTION' | 'ACCOUNT_UPDATE' | 'GENERAL_UPDATE' | 'FOLLOW_UP'
+
+export function getZNSMessageTemplateTag(access_token: string) {
+  return fetch<ZaloOAResponse<ZNSMessageTemplateTag[]>>('message/template-tag', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+  })
+}
+
+export type ZNSQuality = 'HIGH' | 'MEDIUM' | 'LOW' | 'UNDEFINED'
+
+export type ZNSTemplateListResponse = ZaloOAResponse<{
+  templateId: number
+  templateName: string
+  createdTime: number
+  status: 'PENDING_REVIEW' | 'DISABLE' | 'ENABLE' | 'REJECT'
+  templateQuality: ZNSQuality
+}[]> & {
+  metadata: {
+    total: number
+  }
+}
+
+export function getZNSTemplateList(access_token: string, offset: number, limit: number, status?: number) {
+  return fetch<ZNSTemplateListResponse>('template/all', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+    params: {
+      offset,
+      limit,
+      status,
+    },
+  })
+}
+
+export function getZNSSampleData(access_token: string, template_id: string | number) {
+  return fetch<ZaloOAResponse<Record<string, any>>>('template/sample-data', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+    params: {
+      template_id,
+    },
+  })
+}
+
+export interface ZNSRating {
+  note: string
+  rate: number
+  submitDate: string
+  msgId: string
+  feedback: string[]
+  trackingId: string
+}
+
+/**
+ * @param access_token Đoạn mã cần truyền vào để xác minh quyền sử dụng API.
+ * @param template_id ID của template cần lấy thông tin.
+ * @param from_time Thời điểm bắt đầu của khoảng thời gian cần lấy dữ liệu được gửi lên hệ thống. Lưu ý: Định dạng timestamp (đơn vị: millisecond).
+ * @param to_time Thời điểm kết thúc của khoảng thời gian cần lấy dữ liệu được gửi lên hệ thống. Lưu ý: Định dạng timestamp (đơn vị: millisecond).
+ * @param offset Vị trí thứ tự của đánh giá đầu tiên được trả về.
+ * @param limit Số lượng đánh giá tối đa được trả về.
+ */
+export function getZNSRating(access_token: string, template_id: string | number, from_time: number, to_time: number, offset: number, limit: number) {
+  return fetch<ZaloOAResponse<{
+    data: ZNSRating[]
+    total: number
+  }>>('rating/get', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+    params: {
+      template_id,
+      from_time,
+      to_time,
+      offset,
+      limit,
+    },
+  })
+}
+
+export function getZNSQuality(access_token: string) {
+  return fetch<ZaloOAResponse<{
+    oaCurrentQuality: ZNSQuality
+    oa7dayQuality: ZNSQuality
+  }>>('quality', {
+    baseURL: businessBaseURL,
+    headers: { access_token },
+  })
+}
