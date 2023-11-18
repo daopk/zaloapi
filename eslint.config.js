481bc81397e13ddc95b45db6ecfa2dcc40cb5f87
Voi <daopk.dev@gmail.com>
2023-11-18 09:55:49 +0700

    Update devDependencies versions

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,3 @@
-const { ignores } = require('@daopk/eslint-config-ignores')
-const { javascript } = require('@daopk/eslint-config-javascript')
-const { node } = require('@daopk/eslint-config-node')
-const { imports } = require('@daopk/eslint-config-imports')
-const { typescript } = require('@daopk/eslint-config-typescript')
-const { stylistic } = require('@daopk/eslint-config-stylistic')
+import { config } from '@daopk/eslint-preset-ts'
 
-module.exports = [
-  ...ignores(),
-  ...javascript(),
-  ...node(),
-  ...imports(),
-  ...typescript(),
-  ...stylistic(),
-  {
-    files: ['**/*.js'],
-    rules: {
-      '@typescript-eslint/no-require-imports': 'off',
-      '@typescript-eslint/no-var-requires': 'off',
-    },
-  },
-]
+export default config()
