8efdcea99a1024b6b84c9d59dfb8d06eefa7b77a
Voi <daopk.dev@gmail.com>
2023-11-18 10:20:23 +0700

    Revert "Refactor ZaloOAResponse interface"
    This reverts commit d4ed7a8395af7150f0e9c3c7d914f1cc71b9d7c4.

--- packages/oa/src/response.ts
+++ packages/oa/src/response.ts
@@ -1,13 +1,5 @@
-type SuccessResponse<T> = {
-  error: 0;
-  message: string;
-  data: T;
+export interface ZaloOAResponse<T = any> {
+  error: number
+  message: string
+  data?: T
 }
-
-type ErrorResponse = {
-  error: number; // non-zero
-  message: string;
-  data?: unknown;
-}
-
-export type ZaloOAResponse<T = any> = SuccessResponse<T> | ErrorResponse;
